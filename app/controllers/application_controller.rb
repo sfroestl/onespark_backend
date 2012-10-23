ae5f725f7dd31a71b5ebc338409730c893b5275a
sfr <sebastian@froestl.com>
2012-10-23 12:33:27 +0200

    provides HTTP basic auth in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   # helper :sessions, :rest_github, :path, :date
   include SessionsHelper, DateHelper, ApplicationHelper
-  require "digest"
-
-
-  before_filter :authenticate
+  require 'digest'
 
   protected
 
   def authenticate
+    Rails.logger.info "Authenticate filter for Basic Auth"
     authenticate_or_request_with_http_basic do |username, password|
       # you probably want to guard against a wrong username, and encrypt the
       # password but this is the idea.
