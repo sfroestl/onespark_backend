cb8af727070593a1cc0236ab06fbd9ccffe5c415
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-30 13:56:51 +0000

    added 'can't add self as coworker'-constraint

--- app/models/project_coworker.rb
+++ app/models/project_coworker.rb
@@ -15,7 +15,8 @@ class ProjectCoworker < ActiveRecord::Base
   validates :project_id, presence: true
   validates :user_id, presence: true
   validates :permission, presence: true, :inclusion => { :in => [0, 1, 2, 3] }
-  validates_uniqueness_of :user_id, :scope => :project_id
+  validates_uniqueness_of :user_id, :scope => :project_id, :message => "Already in this project."
+  validates_exclusion_of :user_id, :in =>  lambda{|x| x.project ? [ x.project.user_id] : []  }, :message => "It's your project!"
   
   default_scope :order => 'created_at DESC'
 
