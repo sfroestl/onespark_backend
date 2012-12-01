89c6353766a18907a7b3592fe894940081cacc25
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-01 16:59:36 +0000

    serializing coworking status instead of coworkers

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,7 +2,7 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :desc, :due_date, :title
 
   has_one :user, :key => :owner_id, embed: :ids
-  has_many :coworkers, :key => :contributor_ids, embed: :ids #:serializer => UserShortSerializer
+  has_many :project_coworkers, :key => :project_coworker_ids, embed: :ids #:serializer => ProjectCoworkerSerializer
   has_many :tasks, :key => :task_ids, embed: :ids # :serializer => TaskShortSerializer
 
 end
