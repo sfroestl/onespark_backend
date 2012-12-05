704fe5d7c6176522ab1b724f8f0373233bd25b94
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-05 17:13:59 +0000

    fixed another error in creation of coworkers and cleared answer of destroy

--- app/controllers/api/v1/project_coworkers_controller.rb
+++ app/controllers/api/v1/project_coworkers_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::ProjectCoworkersController < Api::V1::ApiController
 
     new_coworker_raw = params[:project_coworker]
     new_coworker_raw.delete(:project_id) #remove project_id after searching, as it is not mass-assginable
-	new_coworker = @project.coworkers.build(new_coworker_raw)	
+	new_coworker = @project.project_coworkers.build(new_coworker_raw)	
     if new_coworker.save
       render json: new_coworker, status: :created
     else
@@ -35,8 +35,7 @@ class Api::V1::ProjectCoworkersController < Api::V1::ApiController
   end
 
   def destroy
-    ProjectCoworker.destroy(params[:id])
-    render json: { message: "ProjectCoworker deleted." }, status: :ok
+    respond_with  ProjectCoworker.destroy(params[:id])
   end
 
 
