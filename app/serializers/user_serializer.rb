9be3628424cd5fc5191b6bc570bef91d65d74921
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-06 14:40:57 +0000

    give out coworker objects instead of users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,7 +4,8 @@ class UserSerializer < ActiveModel::Serializer
 
   # Only authorized user properties
   has_many :projects, :key => :owned_project_ids, :embed => :ids
-  has_many :project_permissions, :key => :collaborated_project_ids, :embed => :ids
+  #has_many :project_permissions, :key => :collaborated_project_ids, :embed => :ids
+  has_many :project_coworkers, :key => :project_coworker_ids, :embed => :ids
   has_many :friends, :key => :contact_ids, :embed => :ids #, :serializer => UserShortSerializer
 
   # Scope for authorized properties
@@ -19,7 +20,10 @@ class UserSerializer < ActiveModel::Serializer
   def include_project_permissions?
     scope == object
   end
-
+  def include_project_coworkers?
+    scope == object
+  end
+  
   def include_friends?
     scope == object
   end
